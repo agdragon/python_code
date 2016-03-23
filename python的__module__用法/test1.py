09af46c1a02161626030f1bc4468693ebecf0af9
xubigshu <664505557@qq.com>
2016-03-23 17:23:58 +0800

    添加笔记

---
+++ python的__module__用法/test1.py
@@ -0,0 +1,11 @@
+import os,sys
+
+class A(object):
+	"""docstring for A"""
+	def __init__(self, arg):
+		super(A, self).__init__()
+		self.arg = arg
+
+# a = A()
+# print type(A.__module__)
+# print A.__module__
---
+++ python的__module__用法/test2.py
@@ -0,0 +1,7 @@
+import os,sys
+
+from test1 import A
+
+# a = A()
+# print type(A.__module__)
+print A.__module__
