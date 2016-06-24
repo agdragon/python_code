f746b2e39fb35e717cb6e7a9b71fa3d6b0f7badf
xubigshu <xudashu@xudashudeMacBook-Pro.local>
2016-06-24 17:40:11 +0800

    添加常用异常处理方式

--- 异常处理/1.py
+++ 异常处理/1.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-class UnknownDB(Exception):
+class UnknownName(Exception):
     """raised for unsupported dbms"""
+    code = 1001
     pass
+
+class Http404(Exception):
+    def __init__(self, msg):
+        super(Http404, self).__init__(msg)
+        self.msg = msg
+        print msg
+
 
 def database():
     name = "xds"
     if name != "xds":
         print name
     else:
-        raise UnknownDB, name
+        raise UnknownName
+
+def database_1():
+    name = "xds"
+    if name != "xds":
+        print name
+    else:
+        raise Http404(name)
+
+
 
 if __name__ == '__main__':
 	try:
-		database()
-	except UnknownDB, e:
-		print e
+		database_1()
+	except Http404:
+		print Http404
